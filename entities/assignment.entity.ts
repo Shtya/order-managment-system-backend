47b407baff2c010ee47f627d4268b1c633fe24e8
amagdykhalil <am2592379@gmail.com>
2026-06-11 15:12:53 +0300

    Update auttomation roles

--- entities/assignment.entity.ts
+++ entities/assignment.entity.ts
@@ -15,8 +15,18 @@ export enum AutoAssignRuleType {
 }
 
 export enum AssignmentStrategy {
-  ROUND_ROBIN = 'roundRobin',
-  LEAST_ACTIVE_ORDERS = 'leastActiveOrders',
+    ROUND_ROBIN = 'roundRobin',
+    LEAST_ACTIVE_ORDERS = 'leastActiveOrders',
+}
+
+export enum WeekDay {
+    SUNDAY = 1 << 0,  // 0001 (1)
+    MONDAY = 1 << 1, // 0010 (2)
+    TUESDAY = 1 << 2, // 0100 (4)
+    WEDNESDAY = 1 << 3, // 1000 (8)
+    THURSDAY = 1 << 4, // 10000 (16)
+    FRIDAY = 1 << 5,  // 100000 (32)
+    SATURDAY = 1 << 6, // 1000000 (64)
 }
 
 @Index(["adminId", "name"], { unique: true })
@@ -49,8 +59,41 @@ export class AutoAssignRuleEntity {
     isActive: boolean;
 
     @Column({ type: 'uuid', nullable: true })
-    lastAssignedEmployeeId?: string;    
+    lastAssignedEmployeeId?: string;
 
+    @Column({
+        type: 'time',
+        nullable: true,
+    })
+    startTime?: string; // e.g. "09:00:00"
+
+    @Column({
+        type: 'time',
+        nullable: true,
+    })
+    endTime?: string; // e.g. "18:00:00"
+
+    @Column({ type: 'int', nullable: true })
+    weekDays?: number;
+
+    @Column({
+        type: 'timestamptz',
+        nullable: true,
+    })
+    activeFrom?: Date;
+
+    @Column({
+        type: 'timestamptz',
+        nullable: true,
+    })
+    activeUntil?: Date;
+
+    
+    @Column({
+        type: "varchar",
+        default: "Africa/Cairo",
+    })
+    timezone: string;
     /**
      * Priority
      * Lower number executes first
@@ -58,7 +101,7 @@ export class AutoAssignRuleEntity {
     @Column({ default: 1 })
     priority: number;
 
-    @Column({type: 'varchar'})
+    @Column({ type: 'varchar' })
     name: string;
 
 
@@ -137,54 +180,54 @@ export class AutoAssignRuleEntity {
 @Entity("order_assignments")
 @Index(["orderId", "isAssignmentActive"]) // Fast lookup to see if an order is "taken"
 export class OrderAssignmentEntity {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
 
-  @Column({ type: 'uuid', })
-  orderId: string;
+    @Column({ type: 'uuid', })
+    orderId: string;
 
-  @ManyToOne(() => OrderEntity)
-  @JoinColumn({ name: "orderId" })
-  order: OrderEntity;
+    @ManyToOne(() => OrderEntity)
+    @JoinColumn({ name: "orderId" })
+    order: OrderEntity;
 
-  @Column({ type: 'uuid', })
-  employeeId: string;
+    @Column({ type: 'uuid', })
+    employeeId: string;
 
-  @ManyToOne(() => User)
-  @JoinColumn({ name: "employeeId" })
-  employee: User;
+    @ManyToOne(() => User)
+    @JoinColumn({ name: "employeeId" })
+    employee: User;
 
-  @Column({ type: 'uuid', })
-  assignedByAdminId: string;
+    @Column({ type: 'uuid', })
+    assignedByAdminId: string;
 
-  @ManyToOne(() => OrderStatusEntity, { eager: true, nullable: true })
-  @JoinColumn({ name: "lastStatusId" })
-  lastStatus: OrderStatusEntity;
+    @ManyToOne(() => OrderStatusEntity, { eager: true, nullable: true })
+    @JoinColumn({ name: "lastStatusId" })
+    lastStatus: OrderStatusEntity;
 
-  @Column({ type: 'uuid', nullable: true })
-  lastStatusId: string;
+    @Column({ type: 'uuid', nullable: true })
+    lastStatusId: string;
 
-  // ✅ Tracking the Work
-  @Column({ type: "int", default: 0 })
-  retriesUsed: number;
+    // ✅ Tracking the Work
+    @Column({ type: "int", default: 0 })
+    retriesUsed: number;
 
-  @Column({ type: "int", default: 3 })
-  maxRetriesAtAssignment: number; // Snapshot of global settings at time of assign
+    @Column({ type: "int", default: 3 })
+    maxRetriesAtAssignment: number; // Snapshot of global settings at time of assign
 
-  @Column({ type: "boolean", default: true })
-  @Index()
-  isAssignmentActive: boolean; // TRUE = Order is "Taken". FALSE = Order is "Free"
+    @Column({ type: "boolean", default: true })
+    @Index()
+    isAssignmentActive: boolean; // TRUE = Order is "Taken". FALSE = Order is "Free"
 
-  // ✅ Timing & Locking
-  @CreateDateColumn({ type: "timestamptz" })
-  assignedAt: Date;
+    // ✅ Timing & Locking
+    @CreateDateColumn({ type: "timestamptz" })
+    assignedAt: Date;
 
-  @Column({ type: "timestamptz", nullable: true })
-  lastActionAt?: Date; // Automatically updates whenever the employee hits 'Retry' or 'Confirm'
+    @Column({ type: "timestamptz", nullable: true })
+    lastActionAt?: Date; // Automatically updates whenever the employee hits 'Retry' or 'Confirm'
 
-  @Column({ type: "timestamptz", nullable: true })
-  lockedUntil?: Date | null; // If now < lockedUntil, employee can see it but can't click it
+    @Column({ type: "timestamptz", nullable: true })
+    lockedUntil?: Date | null; // If now < lockedUntil, employee can see it but can't click it
 
-  @Column({ type: "timestamptz", nullable: true })
-  finishedAt?: Date;
+    @Column({ type: "timestamptz", nullable: true })
+    finishedAt?: Date;
 }
