a643ab439b068cb026958e2c3668fd16f786cb3d
amagdykhalil <am2592379@gmail.com>
2026-06-11 03:07:59 +0300

    Fix

--- dto/user.dto.ts
+++ dto/user.dto.ts
@@ -153,4 +153,8 @@ export class AdminCreateDto {
 	@IsString()
 	@MinLength(6, { message: 'Password must be at least 6 characters' })
 	password?: string;
+
+	@IsOptional()
+	@IsString()
+	adminId?: string;
 }
