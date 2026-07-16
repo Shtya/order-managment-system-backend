8e60239419a0fc2bc274113d455e12e7cafb84b9
amagdykhalil <am2592379@gmail.com>
2026-07-16 12:34:35 +0300

    Update redis configs

--- common/redis/redis.config.ts
+++ common/redis/redis.config.ts
@@ -3,4 +3,6 @@ import { registerAs } from "@nestjs/config";
 export default registerAs('redis', () => ({
   host: process.env.REDIS_HOST as string,
   port: parseInt(process.env.REDIS_PORT as string, 10),
+  password: process.env.REDIS_PASSWORD as string,
+  db: parseInt(process.env.REDIS_DB || '0', 10),
 }));
