11bf937324b190fde3aa427437743ed4ab434cbf
amagdykhalil <am2592379@gmail.com>
2026-08-20 17:59:47 +0300

    Fix ai step

--- src/automation/engine/nodeHandlers.registry.ts
+++ src/automation/engine/nodeHandlers.registry.ts
@@ -724,7 +724,6 @@ export class ActionAiAddressCorrectionHandler extends FlowNodeHandler {
           shippingCompany: config.shippingCompany,
           provider: config.provider,
         },
-        provider: config.provider,
         model: config.modelCode,
         includeDevInfo: true,
         tenantLang: defaultLang,
